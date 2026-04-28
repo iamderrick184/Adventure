402d3f0f0535db4cc6f803fc7d86cf7e26a99fe1
iamderrick184 <kakooza184@gmail.com>
2026-04-28 13:08:59 +0300

    Register models in Django admin with custom admins

--- Monday-django/adventure/isbat/admin.py
+++ Monday-django/adventure/isbat/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
-
+from isbat.models import person,registrtaion
 # Register your models here.
+
+class PersonAdmin(admin.ModelAdmin):
+    list_display=['name']
+
+class RegistrationAdmin(admin.ModelAdmin):
+    list_display=['code']
+
+class ProdAdmin(admin.ModelAdmin):
+    list_display=['prodname','productcat','prodyear']
+
+
+admin.site.register(person,PersonAdmin)
+admin.site.register(registrtaion,RegistrationAdmin)
+admin.site.register(Products,ProdAdmin)
